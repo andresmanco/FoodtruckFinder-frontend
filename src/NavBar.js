7d04478d796e540dba8c38635d24279905bc39cf
github username <github email address>
2018-09-10 13:52:24 -0400

    change navbar divs

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,31 +4,27 @@ import { NavLink } from 'react-router-dom'
 const NavBar = props => {
   return (
     <div className={`ui inverted green menu navbar`}>
-      <a className="item">
+      <NavLink activeClassName="ui active item" className="ui item" exact to="/">
+
         <h2 className="ui header">
           <i className={`truck icon`} />
           <div className="content">Foodtruck Finder</div>
         </h2>
-      </a>
-      <NavLink activeClassName="ui active item" className="ui item" exact to="/">
-        Home
+    
       </NavLink>
-      <NavLink
-        exact
-        to="/signup"
-        className="ui item"
-        activeClassName="ui active item">
-        SignUp
-      </NavLink>
+
       {props.user ? (
-        <div>
+        <a>
           <span className="ui item">{`Logged in as: ${props.user.name}`}</span>
           <NavLink activeClassName="ui active item" className="ui item" exact to="/update">Update your menu</NavLink>
-        </div>
+        </a>
       ) : (
+        <a>
+        <NavLink exact to="/signup" className="ui item" activeClassName="ui active item">SignUp</NavLink>
         <NavLink exact to="/login" className="ui item" activeClassName="ui active item">
           Login
         </NavLink>
+      </a>
       )}
     </div>
   )
